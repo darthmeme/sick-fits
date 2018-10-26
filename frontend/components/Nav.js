4eec1ea4f5b3ae3c37f2ddc5da1383761b14b893
Akira Laine <akira_laine@outlook.com>
2018-10-26 14:21:48 +1100

    get current user, and User render prop component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import NavStyles from '../components/styles/NavStyles'
+import User from './User'
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data }) => {
+        if (data) return <div>{data.me.name}</div>
+      }}
+    </User>
     <Link href="/items">
       <a>Items</a>
     </Link>
